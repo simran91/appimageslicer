23988e685d87d75585e58c577ded08969d809cbb
simran91 <simran@dn.gs>
2015-07-13 02:34:31 +1000

    final beta version... working... but factoring sizes are hardcoded...

--- resize.go
+++ resize.go
@@ -5,12 +5,63 @@ import (
 	"github.com/nfnt/resize"
 	"image"
 	"image/png"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	resizeImage("orig/monk.png", "result.png", 0.2)
+	//
+	// Define the factors we want for the resizings
+	//
+	factors := [...]string{"0.5", "0.25", "1"}
+	
+	//
+	// clean auto-dest directory
+	//
+	err := os.RemoveAll("auto-dest")
+	errorCheck(err)
+
+	//
+	// foreach factor
+	//
+	for _, factor := range factors {
+		//
+		// convert the factor string to a float64
+		//
+ 		factorFloat, err := strconv.ParseFloat(factor, 64)
+ 		errorCheck(err)
+
+		//
+		// Create the factor destination directory
+		//
+		fmt.Println("Factoring for size", factor)
+		destPath := fmt.Sprintf("auto-dest/%sx", factor)
+		err = os.MkdirAll(destPath, 0777)
+		errorCheck(err)		
+
+		//
+		// foreach file in the original directory, resize it!
+		//
+		files, err := ioutil.ReadDir("orig")
+		errorCheck(err)
+
+		for _, file := range files {
+			origFilename := file.Name() // eg. surfboard.png
+			origFilenameWithDir := fmt.Sprintf("orig/%s", origFilename) // eg. "orig/surfboard.png"
+			destFilenameWitDir := fmt.Sprintf("%s/%s", destPath, origFilename)
+
+			//
+			// resize the image
+			//
+			resizeImage(origFilenameWithDir, destFilenameWitDir, factorFloat)
+		}
+
+	}
+
+
+	// resizeImage("orig/monk.png", "result.png", float64(0.2))
 
 }
 
@@ -21,7 +72,7 @@ func resizeImage(inFilename string, outFilename string, factor float64) {
 	//
 	// print a message about what we are converting
 	//
-	fmt.Println("Converting", inFilename, "to", outFilename, "using factor", factor)
+	fmt.Println("\t\t", inFilename, "=>", outFilename, "using factor", factor)
 
 	//
 	// Open the file and read in the image
